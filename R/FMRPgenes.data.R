5bef9b54ea5336d26d246abf331d7ee373097b72
jware@genetics.med.harvard.edu <jware@genetics.med.harvard.edu>
2014-11-07 14:33:45 -0500

    document data

---
+++ R/FMRPgenes.data.R
@@ -0,0 +1,15 @@
+#' FMRP genes
+#'
+#' 842 genes found to interact with the fragile X mental retardation protein (FMRP)
+#'
+#' @name FMRPgenes
+#' @docType data
+#' @author James Ware
+#' @references \url{http://dx.doi.org/10.1038/ng.3050}
+#' @references \url{http://dx.doi.org/10.1016/j.cell.2011.06.013}
+#' @usage data(FMRPgenes)
+#' @keywords datasets
+#' @format A vector of (hgnc) gene identifiers
+NULL
+
+
---
+++ R/autismDeNovos.data.R
@@ -0,0 +1,22 @@
+#' de novo variants found in 1,078 autism trios
+#'
+#' de novo variants found in 1,078 autism trios, published in Nature Genetics(http://www.nature.com/doifinder/10.1038/ng.3050)
+#'
+#' @name autismDeNovos
+#' @docType data
+#' @author James Ware
+#' @references \url{http://dx.doi.org/10.1038/ng.3050}
+#' @usage data(autismDeNovos)
+#' @keywords datasets
+#' @format A data frame with 1096 obs of 2 variables:
+#' \describe{
+#'   \item{gene}{Name of gene containing de novo variant (hgnc)}
+#'   \item{dnmClass}{Fucntional class of variant:
+#'     "syn" = synonymous,
+#'     "mis" = missense,
+#'     "non" = nonsense,
+#'     "splice" = canonical splice site,
+#'     "frameshift" = frameshift indel}
+#' }
+NULL
+
