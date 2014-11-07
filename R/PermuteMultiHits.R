3487391a84f8fc745fe817a31abf6314f5a56189
jware@genetics.med.harvard.edu <jware@genetics.med.harvard.edu>
2014-11-07 17:49:13 -0500

    subFunction

---
+++ R/PermuteMultiHits.R
@@ -0,0 +1,48 @@
+#' Permutes x variants across a genelist, and counts genes with multiple hits
+#'
+#' An internal function called by CalculateMultiHitBurdens
+#'
+#' @param x Total number of de novo variants observed in dataset
+#' @param y Number of genes with >1 de novo variant (of class "class") in the population
+#' @param nperms Number permutations
+#' @param class In c("lof","mis","syn","prot")
+#' @param gene.id Gene identifier used. Currently only hgnc.id
+#' @param include Genes to include in analysis. "all" or a vector of gene names
+#'
+#' @return Returns a named vector of 5 values
+#'
+#' @example
+#' PermuteMultiHits(150,6,class="mis")
+
+PermuteMultiHits <- function(x,y,nperms=100,class="lof", gene.id="hgncID", include.gene="all") {
+
+  #x = total number of DNM observed
+  #y = no of genes with >1 DNM in class of interest
+  #nperms = number permutations, defaults to 100
+  #class = type of DNM assessed, defaults to "lof"
+
+  # Use specified gene ID
+  names(pDNM)[names(pDNM)==gene.id] <- "gene"
+  pDNM$gene <- toupper(as.character(pDNM$gene))
+  include.gene <- toupper(as.character(include.gene))
+
+  # If a list of genes for inclusion is specified, restrict analysis to these genes
+  if(include.gene[1]!="ALL"){
+    pDNM <- pDNM[pDNM$gene %in% include.gene,]
+  }
+  probtable <- pDNM[pDNM$class==class,c("gene","value")]
+  mycounts<- NA
+  for (i in 1:nperms) {
+    DNMsim <- sample(probtable$gene,x,replace=T,prob=probtable$value)
+    mycounts[i] <- length(unique(DNMsim[duplicated(DNMsim)]))
+  }
+  empirical.p<-length(which(mycounts >= y))/nperms
+  #output <- data.frame(matrix(nrow=1,ncol=5))
+  output <- vector(length=5)
+  output <-c(y,mean(mycounts),max(mycounts),empirical.p,x)
+  names(output) <- c("ObsGenes","AvgExpGenes","MaxExpGenes","Empirical.P","TotalObsDNM")
+  return(output)
+}
+
+
+#PermuteMultiHits(150,6,class="splice")
