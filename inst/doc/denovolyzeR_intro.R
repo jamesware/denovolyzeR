cef8b23fc8df8fce00e5bbe96b1c9b50d8cd7bc5
jware@genetics.med.harvard.edu <jware@genetics.med.harvard.edu>
2014-11-13 12:21:08 -0500

    sync vignettes to GitHub, so that they install using devtools::install_github

---
+++ inst/doc/denovolyzeR_intro.R
@@ -0,0 +1,71 @@
+## ------------------------------------------------------------------------
+# Install the package if you haven't already.
+# OPTION 1 - download binary, and install:
+# install.packages("LOCAL/PATH/TO/denovolyzeR.tgz",repos=NULL)  
+
+# OPTION 2 - install from source.  Either download and install, or use devtools:
+# devtools::install_github("jamesware/denovolyzeR")
+
+## ------------------------------------------------------------------------
+library(denovolyzeR)
+# have a look at the example data:
+dim(autismDeNovos)
+head(autismDeNovos)
+
+## ------------------------------------------------------------------------
+denovolyzeByClass(dnm.genes=autismDeNovos$gene,
+                  dnm.classes=autismDeNovos$dnmClass,
+                  nsamples=1078)
+
+## ------------------------------------------------------------------------
+denovolyzeMultiHits(dnm.genes=autismDeNovos$gene,
+                    dnm.classes=autismDeNovos$dnmClass,
+                    nsamples=1078)
+
+
+## ------------------------------------------------------------------------
+denovolyzeMultiHits(dnm.genes=autismDeNovos$gene,
+                    dnm.classes=autismDeNovos$dnmClass,
+                    nsamples=1078,
+                    nperms=1000)
+
+## ------------------------------------------------------------------------
+sum(autismDeNovos$dnmClass %in% c("frameshift","non","splice"))
+
+## ------------------------------------------------------------------------
+denovolyzeMultiHits(dnm.genes=autismDeNovos$gene,
+                    dnm.classes=autismDeNovos$dnmClass,
+                    nsamples=1078,
+                    nperms=1000,
+                    expectedDNMs="expected")
+
+## ------------------------------------------------------------------------
+head(
+denovolyzeByGene(dnm.genes=autismDeNovos$gene,
+                 dnm.classes=autismDeNovos$dnmClass,
+                 nsamples=1078)
+  )
+
+## ----geneset-------------------------------------------------------------
+length(FMRPgenes); head(FMRPgenes)
+
+denovolyzeByClass(dnm.genes=autismDeNovos$gene,
+                  dnm.classes=autismDeNovos$dnmClass,
+                  nsamples=1078,
+                  include.gene=FMRPgenes)
+
+denovolyzeMultiHits(dnm.genes=autismDeNovos$gene,
+                    dnm.classes=autismDeNovos$dnmClass,
+                    nsamples=1078,
+                    nperms=1000,
+                    include.gene=FMRPgenes)
+
+## ----viewDeNovoProbs-----------------------------------------------------
+head(
+  viewDeNovoProbs()
+  )
+
+head(
+  viewDeNovoProbs(format="long")
+  )
+
