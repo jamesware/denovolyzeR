b4f6257f688a782081d4e18f6b44003ebebb0659
jware@genetics.med.harvard.edu <jware@genetics.med.harvard.edu>
2015-04-08 13:08:07 -0400

    a development idea

---
+++ data-raw/annotateMetaClasses.R
@@ -0,0 +1,6 @@
+
+# to replace chunks of code that calculate meta-classes
+
+annotateMetaClasses <- function(x){}
+  # given a list of variant effects, function adds up classes to derive totals for lof, protein-altering, and protein-damaging (=lof + misD)
+  # internal function, not exported
